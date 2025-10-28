bb8621430459abec8aab732665310a94185c5e40
kazne <mixingage1@gmail.com>
2025-10-28 16:16:20 +0900

    作り直し

---
+++ js/site.js
@@ -0,0 +1,12 @@
+﻿window.playSound = (file) => {
+    let audio = new Audio(file);
+    audio.play();
+};
+
+window.flashElement = (id) => {
+    const el = document.getElementById(id);
+    if (!el) return;
+    el.style.transition = "background-color 0.3s";
+    el.style.backgroundColor = "yellow";
+    setTimeout(() => el.style.backgroundColor = "", 500);
+};
